279daa1a716181ca56e87971c35268d5de540a3f
Murillo Oliveira <murillo1047@gmail.com>
2024-01-12 12:58:47 -0300

    feat: position on chess board

---
+++ ChessGame/Chess/ChessPosition.cs
@@ -0,0 +1,26 @@
+﻿using ChessBoard;
+
+namespace Chess
+{
+    internal class ChessPosition
+    {
+        public char Column { get; set; }
+        public int Line { get; set; }
+
+        public ChessPosition(char column, int line)
+        {
+            Column = column;
+            Line = line;
+        }
+
+        public Position toBoardPosition()
+        {
+            return new Position(8 - Line, Column - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + Column + Line;
+        }
+    }
+}
--- ChessGame/Program.cs
+++ ChessGame/Program.cs
@@ -9,19 +9,9 @@ namespace ChessGame
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Board ChessBoard = new Board(8, 8);
-                ChessBoard.PutPiece(new Rook(Color.Black, ChessBoard), new Position(0, 0));
-                ChessBoard.PutPiece(new Rook(Color.Black, ChessBoard), new Position(1, 3));
-                ChessBoard.PutPiece(new King(Color.Black, ChessBoard), new Position(2, 9));
-                ChessBoard.PutPiece(new King(Color.Black, ChessBoard), new Position(0, 0));
-                Screen.showBoard(ChessBoard);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("ERRO: "+ e.Message);
-            }
+            ChessPosition position = new ChessPosition('c', 7);
+            Console.WriteLine(position);
+            Console.WriteLine(position.toBoardPosition());
         }
     }
 }
